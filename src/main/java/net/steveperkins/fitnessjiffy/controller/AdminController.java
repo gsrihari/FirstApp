0e98e5ee175945a74263fa0cb0c508ba05a629b6
gsrihari <g.srihari36@gmail.com>
2014-05-27 10:37:59 +0530

    mm

--- src/main/java/net/steveperkins/fitnessjiffy/controller/AdminController.java
+++ src/main/java/net/steveperkins/fitnessjiffy/controller/AdminController.java
@@ -5,6 +5,7 @@ import net.steveperkins.fitnessjiffy.etl.writer.PostgresWriter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
